95be4d06144e5b23fa41f819d3f3c6a98cc11683
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2020-11-25 14:51:44 +0300

    comments

--- pset3_u/ps3.py
+++ pset3_u/ps3.py
@@ -1,11 +1,8 @@
 # 6.0001 Problem Set 3
 #
 # The 6.0001 Word Game
-# Created by: Kevin Luu <luuk> and Jenna Wiens <jwiens>
-#
-# Name          : <your name>
-# Collaborators : <your collaborators>
-# Time spent    : <total time>
+
+
 
 import math
 import random
