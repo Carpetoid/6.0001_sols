c7c3384ed7a891f534f35096d1f6c35dc1055782
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2021-02-19 22:42:58 +0300

    ps4a

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -22,19 +22,45 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    lst_sequence = list(sequence)
+    
+    if len(sequence) == 1:
+        return lst_sequence
+    
+    return_list = []
+    for i in range(len(lst_sequence)):
+        copy_seq = lst_sequence.copy()
+        current = copy_seq.pop(i)
+        perm_table_for_rest = get_permutations(''.join(copy_seq))
+        for element in perm_table_for_rest:
+            return_list.append(current + element)
+            
+    return return_list   
+        
+        
+    
 
 if __name__ == '__main__':
-#    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
-    
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
+    
+    example_input = 'xyz'
+    print('Input:', example_input)
+    print('Expected Output:', ['xyz', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx'])
+    print('Actual Output:', get_permutations(example_input))
+    
+    
+    example_input = 'olc'
+    print('Input:', example_input)
+    print('Expected Output:', ['olc', 'ocl', 'loc', 'lco', 'col', 'clo'])
+    print('Actual Output:', get_permutations(example_input))
+    
+    example_input = 'hal'
+    print('Input:', example_input)
+    print('Expected Output:', ['hal', 'hla', 'ahl', 'alh', 'lha', 'lah'])
+    print('Actual Output:', get_permutations(example_input))
+    
 
